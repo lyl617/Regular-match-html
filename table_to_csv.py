aa6bf3d6b3174c0e2f210ff9b11d0f867f23fd9b
Jsen617 <18796559783@163.com>
2017-11-29 13:27:53 +0800

    result
    out1 2 3 are results

--- table_to_csv.py
+++ table_to_csv.py
@@ -8,21 +8,19 @@ import re
 import csv
 
 class table_to_csv(object):
-    def __init__(self):
-        pass
     def find_result(self,htmlpage,index):
-        result=[]
-        htmlpage=htmlpage.replace('\n',' ')
-        table_contents=re.findall(r'<table.*?>(.*?)</table.*?>',htmlpage,re.IGNORECASE)
+        result=[]#store the result
+        htmlpage=htmlpage.replace('\n',' ')#replace '\n'  to ' '
+        table_contents=re.findall(r'<table.*?>(.*?)</table.*?>',htmlpage,re.IGNORECASE)#match <table>
         if self.check_type(table_contents):
             for table_content in table_contents:
-                 th_content=re.findall(r'<th.*?>(.*?)</th.*?>',table_content,re.IGNORECASE)
+                 th_content=re.findall(r'<th.*?>(.*?)</th.*?>',table_content,re.IGNORECASE)#match <th>
                  if th_content:
                      result.append(th_content)
-                 tr_contents=re.findall(r'<tr.*?>(.*?)</tr.*?>',table_content,re.IGNORECASE)
+                 tr_contents=re.findall(r'<tr.*?>(.*?)</tr.*?>',table_content,re.IGNORECASE)#match <tr>
                  if self.check_type(tr_contents):
                      for tr_content in tr_contents:
-                         td_content=re.findall(r'<td.*?>(.*?)</td.*?>',tr_content,re.IGNORECASE)
+                         td_content=re.findall(r'<td.*?>(.*?)</td.*?>',tr_content,re.IGNORECASE)#match <td>
                          if td_content:
                              for i in range(len(td_content)):
                                   rest_content=re.findall(r'<.*?>|</.*?>',td_content[i],re.IGNORECASE)
@@ -36,12 +34,23 @@ class table_to_csv(object):
 
         self.write_csv(result,index)
     def check_type(self,findresult):
+        """
+
+        :param findresult: html content
+        :return:
+        """
         if findresult:
             return True
         else:
             raise TypeError('Html type error!')
             return False
     def write_csv(self,contents,i):
+        """
+        write result to csv file
+        :param contents: result
+        :param i: index
+        :return:
+        """
         csvFile=open('out%s.csv'%i,'w')
         writer=csv.writer(csvFile)
         for content in contents:
@@ -49,12 +58,16 @@ class table_to_csv(object):
                 writer.writerow(content)
         csvFile.close()
     def read_html(self,name):
+        """
+        read html file
+        :param name:
+        :return:
+        """
         htmlfile=open(name,'r')
         htmlpage=htmlfile.read()
         return htmlpage
-        print(htmlpage)
 
 if __name__ == "__main__":
     tc=table_to_csv()
-    html_cont=tc.read_html('in1.html')
-    tc.find_result(html_cont,1)
+    html_cont=tc.read_html('in3.html')
+    tc.find_result(html_cont,3)
